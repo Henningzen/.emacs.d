8d4f7d89dada274c3177412dbbc8312be1ede2d6
Henning Jansen <jansenh@Hennings-MacBook-Pro.local>
2023-03-15 11:30:42 +0100

    Add OSX Modifier keys improvement

--- init.el
+++ init.el
@@ -60,6 +60,18 @@
 ;; Are we on a mac?
 (setq is-mac (equal system-type 'darwin))
 
+;; OSX Specific key modifiers
+(when (equal system-type 'darwin)
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'super)
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark))
+  (when (member "Fira Code" (font-family-list))
+    (add-to-list 'initial-frame-alist '(font . "Fira Code-14"))
+    (add-to-list 'default-frame-alist '(font . "Fira Code-14")))
+  (set-fontset-font t 'symbol (font-spec :family "Apple Symbols") nil 'prepend)
+  (set-fontset-font t 'symbol (font-spec :family "Apple Color Emoji") nil 'prepend))
+
 ;; Setup packages
 (require 'setup-package)
 
